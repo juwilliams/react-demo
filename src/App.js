5d7bb2cb25c8f3a65d3b288535083dde9a1e9e70
Justin Williams <justin.williams.dhs@gmail.com>
2020-11-02 15:44:09 -0500

    removing unused things

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import {ThemeProvider} from 'styled-components';
 
 import {RepositoryProvider} from 'state/repository.state';
 
 import RepositoryScreen from 'screens/repository/repository.screen';
 import PullScreen from 'screens/pull/pull.screen';
 
-import {theme} from 'theme';
-
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
-        <RepositoryProvider>
-          <Router>
-            <Route exact path="/repo/:repoId/pulls/:id" component={PullScreen} />
-            <Route exact path="/" component={RepositoryScreen} />
-          </Router>
-        </RepositoryProvider>
-      </ThemeProvider>
+      <RepositoryProvider>
+        <Router>
+          <Route exact path="/repo/:repoId/pulls/:id" component={PullScreen} />
+          <Route exact path="/" component={RepositoryScreen} />
+        </Router>
+      </RepositoryProvider>
     </div>
   );
 };
